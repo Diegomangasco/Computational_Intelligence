6be429b9beacbfb6a7c7990fe42e7467c351a8b1
Diego <diego.gasco99@gmail.com>
2022-12-02 19:34:11 +0100

    GA finished

--- lab3/lib.py
+++ lab3/lib.py
@@ -1,7 +1,7 @@
 import logging
 import random
 from functools import reduce
-from typing import Callable
+from collections import namedtuple
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -24,7 +24,7 @@ class Nim:
         return sum(self._rows) == 0
 
     def verify_state(self) -> bool:
-    # Verify of the new game state is useful for the player with the nim sum rule
+        # Verify of the new game state is useful for the player with the nim-sum rule
         xor = reduce(lambda a, b: a ^ b, self._rows)
         if xor == 0:
             return True
@@ -37,15 +37,16 @@ class RandomPlayer:
         self._moves = []
         self._nMoves = 0
     
-    def play(self, nim: Nim) -> None:
+    def play(self, nim: Nim, trial=0) -> None:
         # Chose a random row and a random number of pieces
         x = random.randint(0, len(nim._rows)-1)
         while nim._rows[x] == 0:
             x = random.randint(0, len(nim._rows)-1)
         y = random.randint(1, nim._rows[x])
         # Update the attributes
-        self._nMoves += 1
-        self._moves.append(f"{y} items from row {x}")
+        if trial == 0:
+            self._nMoves += 1        
+            self._moves.append(f"{y} items from row {x}")
         nim.nimming_remove(x, y)
         
     def printSolution(self) -> None:
@@ -82,59 +83,95 @@ class GeneticAlgorithmSystem:
     def __init__(self) -> None:
         self._nMoves = 0
         self._moves = []
+        self._generations = 50
+        self._population_size = 10
+        self._genome_length = 6
+        self._offspring_size = 100
 
     def printSolution(self) -> None:
         logging.info(f" Genetic Algorithm system won, moves have been: {self._moves}; total: {self._nMoves}")
 
-    def verify_nim_sum(self, ply: tuple, nim: Nim) -> bool:
-        nim.nimming_remove(ply[0], ply[1])
-        valid = nim.verify_state()
-        nim.nimming_add(ply[0], ply[1])
-        return valid
-
-    def strategies(self, nim: Nim, flag: bool) -> dict:
+    def strategies(self, nim: Nim, parameters: list()) -> dict:
         str = dict()
+        x = parameters[0]
+        y = parameters[1]
+        z = parameters[2]
         possible = [(r, o) for r, c in enumerate(nim._rows) for o in range(1, c + 1)]
-        # Calculate possible_nim_sum (and call the verify_nim_sum method), only when necessary
-        if flag:
-            possible_nim_sum = [x for x in possible if self.verify_nim_sum(x, nim) == True]
-            str['nim_sum'] = random.choice(possible_nim_sum) if possible_nim_sum else random.choice(possible)
-        else:
-            str['random'] = random.choice(possible)
-            str['max_from_lowest'] = max(possible, key=lambda m: (-m[0], m[1]))
-            str['min_from_lowest'] = max(possible, key=lambda m: (-m[0], -m[1]))
-            str['max_from_highest'] = max(possible, key=lambda m: (m[0], m[1]))
-            str['min_from_highest'] = max(possible, key=lambda m: (m[0], -m[1]))
-            possible_longest = max((x for x in enumerate(nim._rows)), key=lambda y: y[1])[0]
-            possible_shortest = min((x for x in enumerate(nim._rows) if x[1] > 0), key=lambda y: y[1])[0]
-            str['longest_row'] = random.choice([x for x in possible if x[0] == possible_longest])
-            str['shortest_row'] = random.choice([x for x in possible if x[0] == possible_shortest])
+        flag = False
+        # z is a particular parameter and requires an ad hoc initialization
+        for p in possible:
+            if p[0] == z:
+                # If the row associated with parameter z is still alive
+                flag = True
+                break
+        if flag == False:
+            # Choose another row
+            z = random.choice(possible)[0]
+        possible_z = [p for p in possible if p[0] == z]
+        str = {
+            'x_elements_from_longest': (max(possible, key=lambda i: i[1])[0], 
+            x if x<=max(possible, key=lambda i: i[1])[1] else max(possible, key=lambda i: i[1])[1]),
+            'y_elements_from_smaller': (min(possible, key=lambda i: i[1])[0], 
+            y if y<=min(possible, key=lambda i: i[1])[1] else min(possible, key=lambda i: i[1])[1]),
+            'all_elements_from_row_z': (z, max(possible_z, key=lambda i: i[1])[1])
+        }
         return str
 
-    def choose_strategy(self, nim: Nim, prob: float) -> tuple:
-        p = random.random()
-        if p < prob:
-            s = self.strategies(nim, False)
-            arr = [s['max_from_lowest'], s['max_from_highest'], s['min_from_lowest'], 
-            s['min_from_highest'], s['random']]
-            str = random.choice(arr)
-        else:
-            str = self.strategies(nim, True)['nim_sum']
-        return str
+    def mutation(self, selected, nim: Nim):
+        # Random mutation of all the parameters
+        selected[0] = random.randint(1, len(nim._rows))
+        selected[1] = random.randint(1, len(nim._rows))
+        selected[2] = random.randint(0, len(nim._rows))
+        return selected
 
-    def calculate_prob(self, nim: Nim) -> float:
-        # Idea: increase the probability of choosing a nim-sum strategy if the game is near to the end
-        current_elements = sum(nim._rows)
-        if current_elements <= .3*nim._nelements:
-            # Much probable to choose a nim-sum strategy -> we are close to the end of the game
-            return .3
-        else:
-            # Much probable to choose a dummy strategy -> at the beginning we can do that
-            return .7
+    def game(self, nim: Nim, total_games, player2, parameters: list):
+        # Game simulations with a Random player
+        play = random.choice([1, 2])
+        win = 0
+        par = []
+        for i in range(0, total_games):
+            table = Nim(len(nim._rows))
+            while table.goal() != True:
+                if play == 1:
+                    r = self.strategies(table, parameters)
+                    strategy = random.choice(list(r.values()))
+                    table.nimming_remove(strategy[0], strategy[1])
+                    play = 2
+                else:
+                    player2.play(table, trial=1)
+                    play = 1
+            if play == 2:
+                win += 1
+        return win
 
-    def play(self, nim: Nim):
-        prob = self.calculate_prob(nim)
-        ply = self.choose_strategy(nim, prob)
+    def evolution(self, nim: Nim, player):
+        # Randomic initilization of population
+        population = [list() for _ in range(self._population_size)]
+        for p in population:
+            p.append(random.randint(1, len(nim._rows)))
+            p.append(random.randint(1, len(nim._rows)))
+            p.append(random.randint(0, len(nim._rows)))
+            p.append(0.0)
+        total_games = 20
+        # Loop over generations
+        for generation in range(self._generations):
+            offspring = list()
+            for i in range(self._offspring_size):
+                # Select a gene
+                selected = random.choice(population)
+                if random.random() < 0.7:
+                    # Mutation
+                    selected = self.mutation(selected[0:3], nim)
+                # Game simulation with a certain set of parameters
+                win = self.game(nim, total_games, player, selected[0:3])
+                new_fitness = win/total_games
+                offspring.append([selected[0], selected[1], selected[2], new_fitness])
+            population += offspring
+            population = sorted(population, key=lambda i: i[3], reverse=True)[:self._population_size]
+        return population[0][0:3]
+
+    def play(self, nim: Nim, parameters: list):
+        ply = random.choice(list(self.strategies(nim, parameters).values()))
         nim.nimming_remove(ply[0], ply[1])
         self._nMoves += 1
         self._moves.append(f"{ply[1]} items from row {ply[0]}")
